f8dfc5dda17d25ea01914dd5bf884197cc84498d
lizardcoder9999 <lizardcoder6@gmail.com>
2020-12-24 20:16:47 -0500

    Added user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  lastIp: {
+    type: String,
+    required: true,
+  },
+
+  imageLink: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("users", UserSchema);
